5b1e07f6dc95eee9761e61589d0f2bbe095037a1
cscgrad <haldersg@outlook.com>
2016-08-22 17:30:25 -0400

    Update Math.cs

--- Common/Math.cs
+++ Common/Math.cs
@@ -4,9 +4,29 @@ namespace Analytics.Common
 {
   public static class Math
   {
+    public static bool IsPrime(long n)
+    {
+      bool result = false;
+      double maxP = Math.Sqrt(n);
+       
+      if(n == 2) 
+      { 
+        result = true;
+      }
+      else
+      {
+       for(long i=3; i < maxP; i+=2)
+       {
+         if(n%i == 0) { result = false; break; }
+       }
+      }
+      
+      return result;
+    }
+    
     public static void Main(string[] args)
     {
-      Console.WriteLine("Hello");
+      Console.WriteLine("IsPrime(17): " + IsPrime(17));
     }
   }
 }
